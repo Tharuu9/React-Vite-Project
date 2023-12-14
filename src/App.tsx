e9600f8ec8906b60c2252e1bfe82492c51188007
Tharuu Wijesekara <114207360+Tharuu9@users.noreply.github.com>
2023-12-14 21:05:47 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import './App.css'
+
+
+class App extends React.Component<any, any>{
+
+    state = {
+        count:0 ,
+        visible: true
+    };
+
+  /*  up = (): void => {
+        this.setState({count: this.state.count + 1})
+}
+
+
+    down = (): void => {
+        this.setState({count: this.state.count - 1})
+    }*/
+
+    update = (type: string): void => {
+        switch(type){
+            case 'UP':
+            this.setState({count: this.state.count + 1});
+            break;
+            default:
+                this.setState({count: this.state.count - 1});
+                break;
+
+        }
+}
+
+render(){
+        return(
+
+            <div className="m-5">
+
+                <button className="inline bg-green-600 px-5" onClick={this.up}>+</button>
+                <div className="inline mx-5 font-bold">{this.state.count}</div>
+                <button className="inline bg-red-600 px-5" onClick={this.down}>-</button>
+            </div>
+        );
+    }
+}
+
+export default App
