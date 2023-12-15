297a282dae0afdfc9b5f5e9285b597b3a9e306fe
Tharuu Wijesekara <114207360+Tharuu9@users.noreply.github.com>
2023-12-15 16:08:57 +0530

    Content update

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -5,13 +5,13 @@ class Footer extends React.Component<any,any> {
         return(
 
             <footer className={'bg-teal-950 text-white p-5'}>
-                <img   src="" title="logo" alt="logo" className={'w-[50px]'}/>
+                <img   src="src/assets/logo t.png" title="logo" alt="logo" className={'w-[50px]'}/>
                 <ul className={'mt-5'}>
-                    <li>abcddd</li>
-                    <li>dffef</li>
-                    <li>fcdesff</li>
+                    <li>IJSE</li>
+                    <li>GDSE</li>
+                    <li>RAD</li>
                 </ul>
-                <div className={'mt-5 text-center'}>Copy @ 2023 Blog LK</div>
+                <div className={'mt-5 text-center'}>Copy @2023 Blog LK</div>
             </footer>
         );
     }
